739e8ec9b57450af32cea8a0bf9ca67e517772a0
Damtev <damtev@yandex.ru>
2021-03-24 12:12:51 +0300

    Added 1.2

--- linear_search.py
+++
@@ -1,9 +0,0 @@
-def linear_search(f, left, delta = 1e-2, eps = 1e-3, factor = 2):
-  start_value = f(left)
-  cur_delta = delta
-  right = left + cur_delta
-  while f(right) <= start_value + eps:
-    cur_delta *= 2
-    right += cur_delta
-
-  return right
